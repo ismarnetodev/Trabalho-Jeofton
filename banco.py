6e43be4a8e752c1a99f8a58371bdb61e9057eff9
ismarnetodev <dsxzz41@gmail.com>
2025-05-12 20:25:12 -0300

    Pagina inicial

--- banco.py
+++ banco.py
@@ -1,1 +1,22 @@
-print("Ola, mundo!") 
+import customtkinter as ctk
+import tkinter as tk
+
+ctk.set_appearance_mode('dark')
+
+def apesnas_leitura(entry_value):
+    return entry_value.isalpha() or entry_value == "" 
+
+def validar_login():
+    usuario = campo_nome.get()
+    senha = campo_password.get()
+
+    if usuario == "Ismar" and  senha == "te amo":
+        verdade.configure(text='Login feito com sucesso!', text_color="green")
+    else:
+        verdade.configure(text='Login negado!', text_color="red")
+
+app = ctk.CTk()
+app.title("Sistema de Login da carteira")
+app.geometry('300x300')
+
+app.mainloop()
