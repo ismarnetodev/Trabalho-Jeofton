133cba1bfaca61286ddd709c292e00e254b281ab
augustoluizdev <luizaugustolimadantas@gmail.com>
2025-05-15 17:30:16 -0300

    Página de login e inicio do conversor de moedas

--- banco.py
+++ banco.py
@@ -30,15 +30,22 @@ def cadastrar_usuario(usuario, senha):
     salvar_usuarios(usuarios)
     return True
 
-def apesnas_leitura(entry_value):
+def apenas_leitura(entry_value):
     return entry_value.isalpha() or entry_value == "" 
 
 def validar_login():
     usuario = campo_nome.get()
     senha = campo_password.get()
 
-    if cadastrar_usuario(usuario, senha):
-        verdade.configure(text='Login feito com sucesso!', text_color="green")
+    if verificar_login(usuario, senha):
+        verdade.configure()
+        campo_user.pack_forget()
+        campo_nome.pack_forget()
+        campo_senha.pack_forget()
+        campo_password.pack_forget()
+        botoao_login.pack_forget()
+        botao_cadastro.pack_forget()
+        pagina_conversor.pack(pady=20)
     else:
         verdade.configure(text='Login negado!', text_color="red")
 
@@ -51,11 +58,14 @@ def cadastrar():
     else:
         verdade.configure(text="O usuario já existe!", text_color='orange')
 
+
 app = ctk.CTk()
 app.title("Sistema de Login da carteira")
 app.geometry('300x300')
  
-vcmd = app.register(apesnas_leitura)
+vcmd = app.register(apenas_leitura) 
+
+pagina_conversor= ctk.CTkFrame(app)
 
 campo_user = ctk.CTkLabel(app, text='Usuário:')
 campo_user.pack(pady=5)
@@ -78,6 +88,18 @@ botao_cadastro.pack(pady=5)
 verdade = ctk.CTkLabel(app, text='')
 verdade.pack(pady=10)
 
+titulo= ctk.CTkLabel(pagina_conversor, text="Conversor de moedas")
+texto_moeda_origem= ctk.CTkLabel(pagina_conversor, text="Selecione a moeda de origem")
+texto_moeda_destino= ctk.CTkLabel(pagina_conversor, text="Selecione a moeda de destino")
 
+campo_moeda_origem= ctk.CTkOptionMenu(pagina_conversor, values=["USD", "BRL", "EUR", "BTC"] )
+campo_moeda_destino= ctk.CTkOptionMenu(pagina_conversor, values=["USD", "BRL", "EUR", "BTC"] )
+
+titulo.pack(padx=10, pady=10)
+texto_moeda_origem.pack(padx=10, pady=10)
+campo_moeda_origem.pack(padx=10, pady=10)
+texto_moeda_destino.pack(padx=10, pady=10)
+campo_moeda_destino.pack(padx=10, pady=10)
 
 app.mainloop()
+
